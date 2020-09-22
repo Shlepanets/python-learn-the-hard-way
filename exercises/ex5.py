89b5b657fd6e78da7e6444294aff421063a68af9
Shlepanets <Shlepanets@gmail.com>
2020-09-23 00:34:21 +0300

    added exercises 4-12

---
+++ exercises/ex5.py
@@ -0,0 +1,18 @@
+my_name = 'Shawn'
+my_age = 18
+my_height = 178 # cm
+my_weight = 75 # kg
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+print(f"Let's talk about {my_name}.")
+print(f"He's {my_height} centimeters tall.")
+print(f"He's {my_weight} kilogramms heavy.")
+print("Actually, that's not too heavy.")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} depending on the coffee.")
+
+# Some tricky line
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_height}, and {my_weight} I get {total}.")
---
+++ exercises/ex9.py
@@ -0,0 +1,14 @@
+# Some new stuff
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print("Here are the days: ", days)
+print("Here are the months: ", months)
+
+print("""
+Wow, probably
+I can type
+On several lines
+without special symbols
+""")
